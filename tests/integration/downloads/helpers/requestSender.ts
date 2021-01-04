41e4ef97cd1437540bc14eb6bd1179164d7b407a
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-04 12:58:57 +0200

    test(s3): added integration test

--- tests/integration/downloads/helpers/requestSender.ts
+++ tests/integration/downloads/helpers/requestSender.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as supertest from 'supertest';
 import { Application } from 'express';
 
@@ -11,6 +12,22 @@ export async function init(): Promise<void> {
   app = await builder.build();
 }
 
-export async function download(directory: string, file:string): Promise<supertest.Response> {
-  return supertest.agent(app).get(`/download/${directory}/${file}`);
+export async function download(key: string, parseFile = false): Promise<supertest.Response> {
+  let res = supertest.agent(app).get(`/download/${key}`);
+  if (parseFile) {
+    res = res.buffer(true).parse(binaryParser);
+  }
+  return res;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const binaryParser = (res: supertest.Response, fn: (err: Error | null, body: any) => void): void => {
+  const data: any[] = []; // Binary data needs binary storage
+
+  res.on('data', (chunk) => {
+    data.push(chunk);
+  });
+  res.on('end', () => {
+    fn(null, Buffer.concat(data).toString());
+  });
+};
