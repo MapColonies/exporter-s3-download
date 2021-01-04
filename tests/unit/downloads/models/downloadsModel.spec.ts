8f6e74291062a2061b7c3753036d487477532e48
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-04 09:33:19 +0200

    test(s3): added download unit test

--- tests/unit/downloads/models/downloadsModel.spec.ts
+++ tests/unit/downloads/models/downloadsModel.spec.ts
@@ -1,6 +1,8 @@
-import { IConfig } from 'config';
+/* eslint-disable jest/no-mocks-import */
+import { IConfig, IUtil } from 'config';
 import { IS3Config } from '../../../../src/downloads/interfaces';
 import { DownloadsManager } from '../../../../src/downloads/models/downloadsManager';
+import { s3Mocks } from '../../../__mocks__/aws-sdk';
 
 let downloadsManager: DownloadsManager;
 const s3Config: IS3Config ={
@@ -12,26 +14,48 @@ const s3Config: IS3Config ={
 const configMock: IConfig = {
   get: jest.fn().mockReturnValue(s3Config),
   has: jest.fn(),
-  util: undefined
+  util: {} as unknown as IUtil
 }
 
 describe('DownloadsManager', () => {
   beforeEach(function () {
     downloadsManager = new DownloadsManager({ log: jest.fn() }, configMock );
+    s3Mocks.s3.mockClear();
+    s3Mocks.getObject.mockClear();
+    s3Mocks.createReadStream.mockClear();
+    jest.mock('aws-sdk', () => {
+      return {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          S3: s3Mocks.s3
+      };
+    });
   });
 
   describe('download', () => {
-    //TODO: mock s3
-    it('return the resource of id 1', function () {
-      const fileName = 'testFile'
+    it('return the requested file', function () {
+      const fileName = 'testFile';
+      const directory = 'testDir';
+      const fileContent = 'content';
+
+      s3Mocks.createReadStream.mockReturnValueOnce(fileContent)
+
       // action
-      const resource = downloadsManager.download({
-        directory:'testDir',
+      const file = downloadsManager.download({
+        directory: directory,
         fileName: fileName
       });
 
       // expectation
-      expect(resource.name).toEqual(fileName);
+      expect(s3Mocks.getObject).toHaveBeenCalledTimes(1);
+      expect(s3Mocks.getObject).toHaveBeenCalledWith({ 
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Bucket: s3Config.bucket,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Key: `${s3Config.bucket}/${directory}/${fileName}`
+      });
+      expect(s3Mocks.createReadStream).toHaveBeenCalledTimes(1);
+      expect(file.name).toEqual(fileName);
+      expect(file.contentStream).toEqual(fileContent);
       
     });
   });
